2b4419ee0980925027a003f6e225313baf52f92f
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-07-09 20:40:18 +0530

    Time: 0 ms (100%), Space: 18 MB (76.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& image, int sr, int sc, int color) {
-        int scol = image[sr][sc];
-        if(scol==color) return;
-        image[sr][sc] = color;
-        int m = image.size(), n = image[0].size();
-        int i, j, row, col;
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        while (!q.empty()) {
-            sr = q.front().first;
-            sc = q.front().second;
+    void dfs(vector<vector<int>>& image, int r, int c, int color, int scol) {
+        if(r<0 || c<0 || r>=image.size() || c>=image[0].size() || image[r][c]!=scol)
+            return;
+        
+        image[r][c] = color;
 
-            q.pop();
-
-            for (i = -1; i <= 1; i++) {
-                for (j = -1; j <= 1; j++) {
-                    if (abs(i) == abs(j))
-                        continue;
-                    row = abs(sr + i);
-                    col = abs(sc + j);
-                    if (row < m && col < n && image[row][col] == scol) {
-                        image[row][col] = color;
-                        q.push({row,col});
-                    }
-                }
-            }
-        }
+        dfs(image, r-1, c, color,scol);
+        dfs(image, r+1, c, color,scol);
+        dfs(image, r, c-1, color,scol);
+        dfs(image, r, c+1, color,scol);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
-        int scol = image[sc][sr];
-        bfs(image, sr, sc, color);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int scol = image[sr][sc];
+        if(scol!=color) dfs(image,sr,sc,color,scol);
         return image;
     }
 };
