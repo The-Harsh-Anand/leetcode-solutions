ea4b6e774c14840d2cc705ed59aeeb3ab4543eaf
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-07-19 16:38:53 +0530

    Time: 0 ms (100%), Space: 14 MB (75.72%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+    void dfs(vector<vector<char>>& board, int row, int col) {
+        board[row][col]='B';
+        int i,m=board.size(),n=board[0].size();
+        for(i=0;i<4;i++) {
+            int r = abs(row+dir[i][0]);
+            int c = abs(col+dir[i][1]);
+            if(r<m && c<n && board[r][c]=='O')
+                dfs(board,r,c);
+        }
+    }
     void solve(vector<vector<char>>& board) {
         queue<vector<int>> q;
         int i,j,m=board.size(),n=board[0].size();
-        for(i=0;i<m;i++) {
-            for(j=0;j<n;j++) {
-                if(board[i][j]=='O' && (i==0||j==0||i==m-1||j==n-1)) {
-                    q.push({i,j});
-                    board[i][j]='B';
-                }
-            }
+        for(j=0;j<n;j++) {
+            if(board[0][j]=='O') dfs(board,0,j);
+            if(board[m-1][j]=='O') dfs(board,m-1,j);
         }
-        //MULTISOURCE BFS
-        vector<vector<int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-        while(!q.empty()) {
-            int row = q.front()[0];
-            int col = q.front()[1];
-            q.pop();
-            for(i=0;i<4;i++) {
-                int r = abs(row+dir[i][0]);
-                int c = abs(col+dir[i][1]);
-                if(r<m && c<n && board[r][c]=='O') {
-                    board[r][c] = 'B';
-                    q.push({r,c});
-                } 
-            }
+        for(j=0;j<m;j++) {
+            if(board[j][0]=='O') dfs(board,j,0);
+            if(board[j][n-1]=='O') dfs(board,j,n-1);
         }
-
         for(i=0;i<m;i++) {
             for(j=0;j<n;j++) {
                 if(board[i][j]=='O') board[i][j]='X';
