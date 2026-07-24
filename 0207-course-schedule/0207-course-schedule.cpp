440b1dff3602fa90f9d80fecd603d5c30f84c885
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-07-24 19:48:08 +0530

    Time: 1 ms (83.1%), Space: 19.1 MB (81.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,29 +2,23 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& pr) {
         vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses,0);
         int i;
-        for(auto i:pr) {
+        for(auto &i:pr) {
             adj[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
         }
-        vector<int> vis(numCourses,0), path(numCourses,0);
-        for(i=0;i<numCourses;i++) {
-            if(!vis[i]) {
-                if(dfs(adj,vis,i,path)) return false;
+        queue<int> q;
+        for(i=0;i<numCourses;i++) if(indegree[i]==0) q.push(i);
+        while(!q.empty()) {
+            int n = q.front();
+            q.pop();
+            for(auto &it: adj[n]) {
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
+        for(auto &it: indegree) if(it>0) return false;
         return true;
-    }
-    bool dfs(vector<vector<int>>& adj, vector<int>& vis, int src, vector<int>& path) {
-        vis[src] = path[src] = 1;
-        int i;
-        for(auto i:adj[src]) {
-            if(!vis[i]) {
-                if(dfs(adj,vis,i,path)) return true;
-            } else if(path[i]){
-                return true;
-            }
-        }
-        path[src] = 0;
-        return false;
     }
 };
