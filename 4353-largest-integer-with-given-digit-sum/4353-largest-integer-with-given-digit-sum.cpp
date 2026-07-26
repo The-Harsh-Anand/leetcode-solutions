3701814db03a005b70fa5f3200acda7825079a83
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-07-26 12:32:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (53.1%) - LeetHub

---
+++ 4353-largest-integer-with-given-digit-sum/4353-largest-integer-with-given-digit-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestInteger(int n, int s) {
+        if(s>9*n) return -1;
+
+        long long ans=0;
+        for(int i=0;i<n;i++) {
+            int d = min(9,s);
+            ans = ans*10 + d;
+            s-=d;
+        }
+        return ans;
+    }
+};
