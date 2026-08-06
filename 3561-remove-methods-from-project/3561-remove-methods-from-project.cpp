8d95707cc1b67bdda826d305f61aaeadbe83d3e5
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-08-06 11:07:04 +0530

    Time: 159 ms (86.39%), Space: 308.3 MB (62.25%) - LeetHub

--- 3561-remove-methods-from-project/3561-remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/3561-remove-methods-from-project.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj,vector<int>& sus, int node) {
+    void dfs(vector<vector<int>>& adj,vector<int>& sus, int& node) {
         sus[node]=-1;
         for(auto it:adj[node]) {
             if(!sus[it]) dfs(adj,sus,it);
-        }
-    }
-    void helper(vector<vector<int>>& adj, vector<int>& sus, int node) {
-        sus[node]=1;
-        for(auto it:adj[node]) {
-            if(!sus[it] || sus[it]==-1) {
-                helper(adj,sus,it);
-            } 
         }
     }
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& inv) {
         vector<vector<int>> adj(n);
         vector<int> sus(n,0);
         int i,flag=0;
-        for(i=0;i<inv.size();i++) {
-            adj[inv[i][0]].push_back(inv[i][1]);
+        for(auto& it:inv) {
+            adj[it[0]].push_back(it[1]);
         }
+
         dfs(adj,sus,k);
         vector<int> ans;
 
-        for(auto it:inv) {
+        for(auto& it:inv) {
             if(!sus[it[0]] && sus[it[1]]) {
                 for(i=0;i<n;i++) ans.push_back(i);
                 return ans;
             }
         }
+        
         for(i=0;i<n;i++) {
             if(!sus[i]) ans.push_back(i);
         }
