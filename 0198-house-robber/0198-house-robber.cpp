5c3b8757298a5f3371f93fe4749bee0fbf18eb04
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-08-11 18:25:10 +0530

    Time: 0 ms (100%), Space: 10 MB (90.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,12 +3,12 @@ public:
     int rob(vector<int>& nums) {
         int i,n=nums.size();
         if(n==0) return 0;
-        vector<int> dp(n+1,0);
-        dp[0] = 0;
-        dp[1] = nums[0];
+        int x = 0, y = nums[0],z=y;
         for(i=1;i<n;i++) {
-            dp[i+1] = max(dp[i-1]+nums[i],dp[i]);
+            z = max(x+nums[i],y);
+            x=y;
+            y=z;
         }
-        return dp[n];
+        return z;
     }
 };
