bf464fa781034645fe98c8bd324728ef07b1c95b
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-08-24 21:46:58 +0530

    Time: 0 ms (100%), Space: 14.6 MB (85.15%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int i,j,n=matrix.size(),ans=INT_MAX;
-        if(n==1) return matrix[0][0];
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(i=0;i<n;i++) {
-            for(j=0;j<n;j++) {
-                if(i==0) {
-                    dp[i][j]=matrix[i][j];
-                } else {
-                    if(j==0) dp[i][j] = min({dp[i-1][j],dp[i-1][j+1]})+matrix[i][j];
-                    else if(j==n-1) dp[i][j] = min({dp[i-1][j-1],dp[i-1][j]})+matrix[i][j];
-                    else dp[i][j] = min({dp[i-1][j],dp[i-1][j-1],dp[i-1][j+1]})+matrix[i][j];
+        int i, j, n = matrix.size(), ans = INT_MAX;
+        if (n == 1)
+            return matrix[0][0];
+        vector<int> dp(matrix[0].begin(), matrix[0].end());
+        int left = INT_MAX, right = INT_MAX, center = INT_MAX;
+        for (i = 1; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (j == 0) {
+                    left = INT_MAX;
+                    center = dp[j];
+                    right = dp[j + 1];
                 }
-                if(i==n-1) ans = min(ans,dp[i][j]);
+                dp[j] = min({left, center, right}) + matrix[i][j];
+                left = center;
+                center = right;
+                if (j < n - 2)
+                    right = dp[j + 2];
+                else
+                    right = INT_MAX;
+                if (i == n - 1)
+                    ans = min(ans, dp[j]);
             }
         }
         return ans;
