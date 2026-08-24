f7d3d75c77a65a54ffb83ba0d96b5551714051d0
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-08-25 01:56:54 +0530

    Time: 103 ms (9.74%), Space: 18.5 MB (48.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,26 +11,32 @@
 class Solution {
 
 private:
-    struct comp {
-        bool operator()(const ListNode* l1, const ListNode* l2) {
-            return l1->val > l2->val;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode(-1);
+        ListNode* temp = result;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                temp->next = l1;
+                l1 = l1->next;
+            } else {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
         }
-    };
+        temp->next = l1?l1:l2;
+        return result->next;
+    }
+
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,comp> pq;
-        for(auto& list:lists) {
-            if(list) pq.push(list);
+        int n = lists.size(),i;
+        if(n==0) return NULL;
+        else if(n==1) return lists[0];
+        ListNode* temp = lists[0];
+        for(i=1;i<n;i++) {
+            temp = mergeTwoLists(temp,lists[i]);
         }
-        ListNode* result = new ListNode(-1);
-        ListNode* temp=result;
-        while(!pq.empty()) {
-            ListNode* curr = pq.top();
-            pq.pop();
-            temp->next = curr;
-            if(curr->next) pq.push(curr->next);
-            temp = curr;
-        }
-        return result->next;
+        return temp;
     }
 };
