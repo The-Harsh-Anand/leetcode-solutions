3f04ba7c040b9da73c596b1cfea31f9c643162ec
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-08-25 18:41:00 +0530

    Time: 0 ms (100%), Space: 24 MB (94.59%) - LeetHub

--- 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int i;
-        set<int> hash(nums.begin(),nums.end());
-        for(i=k;hash.contains(i);i+=k);
+        int i,n=nums.size();
+        vector<bool> hash(101,false);
+        for(i=0;i<n;i++)  hash[nums[i]]=true;
+        for(i=k;i<=100 && hash[i];i+=k);
         return i;
     }
 };
