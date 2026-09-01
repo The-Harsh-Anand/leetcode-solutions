8dd637df83947b4aaa30842cd88eccb6f13c2e09
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-09-01 22:24:39 +0530

    Time: 0 ms (100%), Space: 22.8 MB (47.38%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.cpp
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int n=nums.size(),i,j,flag=0;
-        vector<int> hash(k+1,0);
+        unordered_set<int> st;
         for(i=n-1;i>=0;i--) {
-            if(nums[i]>k) continue;
-            hash[nums[i]]=1;
-            flag=1;
-            for(j=1;j<=k;j++) {
-                if(hash[j]==0) flag=0;
-            }
-            if(flag==1) break;
+            if(nums[i]<=k) st.insert(nums[i]);
+            if(st.size()==k) break;
         }
         return n-i;
     }
