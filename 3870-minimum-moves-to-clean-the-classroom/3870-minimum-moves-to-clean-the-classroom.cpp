0b24c10bd1b55dfd35f5cfe76103a9f5ba317c98
Harsh Anand <98966038+The-Harsh-Anand@users.noreply.github.com>
2026-09-01 12:53:09 +0530

    Time: 183 ms (93.88%), Space: 123.6 MB (87.76%) - LeetHub

--- 3870-minimum-moves-to-clean-the-classroom/3870-minimum-moves-to-clean-the-classroom.cpp
+++ 3870-minimum-moves-to-clean-the-classroom/3870-minimum-moves-to-clean-the-classroom.cpp
@@ -1,56 +1,74 @@
 class Solution {
+private:
+    struct st {
+        int row, col, mask, eng;
+    };
+
 public:
     int minMoves(vector<string>& classroom, int energy) {
-        int m=classroom.size();
-        int n=classroom[0].size();
+        int m = classroom.size();
+        int n = classroom[0].size();
 
-        int total=0,sr=0,sc=0,i,j;
-        vector<vector<int>> id(m,vector<int>(n,-1));
+        int total = 0, sr = 0, sc = 0, i, j;
+        vector<vector<int>> id(m, vector<int>(n, -1));
 
-        for(i=0;i<m;i++) {
-            for(j=0;j<n;j++) {
-                if(classroom[i][j]=='S') {
-                    sr=i;sc=j;
-                } else if(classroom[i][j]=='L') {
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                if (classroom[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                } else if (classroom[i][j] == 'L') {
                     id[i][j] = total++;
                 }
             }
         }
 
-        int mask = 1<<total; //all the litters represented in bits
-        int fullMask = mask-1;
+        int masks = 1 << total; // all the litters represented in bits
+        int fullMask = masks - 1;
 
-        vector<vector<vector<int>>> bestEng(m,vector<vector<int>>(n,vector<int>(mask,-1)));
-        vector<int> dir = {0,-1,0,1,0};
-        queue<vector<int>> q;
-        q.push({sr,sc,0,energy,0}); 
-        //q{start_row, start_col, collected_litter, curr_energy, steps}
+        vector<int> bestEng(m * n * masks, -1);
+        int dir[] = {0, -1, 0, 1, 0};
+        queue<st> q; // q{start_row, start_col, collected_litter, curr_energy}
+        q.push({sr, sc, 0, energy});
+        bestEng[(sr * n + sc) * masks] = energy;
+        int steps = 0;
 
-        while(!q.empty()) {
-            vector<int> state = q.front();
-            q.pop();
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz--) {
+                auto [r, c, mask, eng] = q.front();
+                q.pop();
 
-            if(state[2]==fullMask) return state[4];
-            if(state[3]==0) continue;
+                if (mask == fullMask)
+                    return steps;
+                if (eng == 0)
+                    continue;
 
-            for(i=0;i<4;i++) {
-                int nr = abs(state[0]+dir[i]);
-                int nc = abs(state[1]+dir[i+1]);
+                for (i = 0; i < 4; i++) {
+                    int nr = r + dir[i];
+                    int nc = c + dir[i + 1];
 
-                if(nr>=m || nc>=n || classroom[nr][nc]=='X') continue;
+                    if (nr < 0 || nc < 0 || nr >= m || nc >= n ||
+                        classroom[nr][nc] == 'X')
+                        continue;
 
-                int neng = state[3]-1;
-                int nMask = state[2];
+                    int neng = eng - 1;
+                    int nMask = mask;
 
-                char cell = classroom[nr][nc];
-                if(cell=='L')   nMask = nMask|(1<<id[nr][nc]);
-                else if(cell=='R')   neng = energy;
+                    if (classroom[nr][nc] == 'L')
+                        nMask = nMask | (1 << id[nr][nc]);
+                    else if (classroom[nr][nc] == 'R')
+                        neng = energy;
 
-                if(bestEng[nr][nc][nMask]>=neng) continue;
+                    int idx = (nr * n + nc) * masks + nMask;
+                    if (bestEng[idx] >= neng)
+                        continue;
 
-                bestEng[nr][nc][nMask] = neng;
-                q.push({nr,nc,nMask,neng,state[4]+1});
+                    bestEng[idx] = neng;
+                    q.push({nr, nc, nMask, neng});
+                }
             }
+            steps++;
         }
         return -1;
     }
